b36b50166fab68178e9f9c73b3efa1913b2bfc48
Tim Mckenzie <timmckenzie@gmail.com>
2016-03-24 23:04:19 -0700

    Moved command application to bin/cli.js, index.js now returns HarServer class.
    Fits better into npm/nodejs model.

--- index.js
+++ index.js
@@ -1,35 +1,3 @@
-var fs = require('fs');
-var Promise = require('bluebird');
-
-Promise.promisifyAll(fs);
-
-var argLib = require('./lib/commandArgument');
-var harServer = require('./lib/harServer');
-
-var args;
-
-args = argLib.args();
-
-if (args.help) {
-    argLib.printHelp();
-    process.exit(0);
-}
-
-args.harFileName = args._[0];
 
 
-var server = harServer(args);
-server.run().catch(function (err) {
-    console.error('Error ' + err.toString());
-    process.exit(1);
-
-});
-
-if (args.removeHostFileEntries) {
-    process.on('SIGINT', function () {
-        server.cleanHostFile().then(function () {
-            process.exit(0);
-        });
-
-    });
-}
+module.exports = require('./lib/harServer');
